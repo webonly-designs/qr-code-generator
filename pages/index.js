ccba583b193289a637943637802c5cf6ba61bcc3
webonly-designs <webonly.designs@gmail.com>
2023-03-22 18:09:54 -0500

    qr code generator working

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
+import QRCode from "react-qr-code"
 
 export default function Home() {
+  const [value, setValue] = useState("https://www.google.com")
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="QR Code Generator" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>QR Code Generator</title>
       </Head>
-      <main className={styles.main}>
-       
+      <main className="flex justify-center items-center w-full h-screen bg-black">
+       <div className='space-y-4 flex flex-col items-center justify-center'>
+        <QRCode value={value} />
+        <form>
+          <input
+            type="text" 
+            placeholder='Enter the link to generate a QR Code'
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className="bg-black/10 border rounded-md w-96 p-2 text-gray-100"
+          />
+        </form>
+       </div>
       </main>
     </>
   )
